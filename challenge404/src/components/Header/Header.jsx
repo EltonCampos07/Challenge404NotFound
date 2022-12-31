ab5e06922b2997683463bd5d0cc42c41ebd5ca1e
EltonCampos07 <eltonapcnunes@gmail.com>
2022-12-31 11:55:52 -0300

    finalização do header

---
+++ challenge404/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <h2 className="header-paragraph">404 not found</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
