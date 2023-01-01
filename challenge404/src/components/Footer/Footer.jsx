b99770d971c4e75e2e58935bfcd3666c2848cf09
EltonCampos07 <eltonapcnunes@gmail.com>
2022-12-31 22:48:29 -0300

    Atualização do Header e criação do Footer

---
+++ challenge404/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p className="footer-paragraph">
+        created by <span>Elton Campos</span> - devChallenges.io
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
